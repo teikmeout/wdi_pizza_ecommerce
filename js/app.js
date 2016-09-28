6860a4b5ab010afb74c50e093ae42b65db5e154a
Dawa Sherpa <dasherpa7@gmail.com>
2016-09-28 11:05:52 -0400

    some js notes

--- js/app.js
+++ js/app.js
@@ -3,7 +3,33 @@ document.addEventListener('DOMContentLoaded', function() {
 
   var $listForm = document.querySelector('.list-maker-form');
 
-
 });
 
 console.log('this is before the dom loaded');
+
+
+/* Elements to use
+Test elements in safari
+
+- form
+- button
+- input type=checkbox (for checkbox)
+- input type=range (for slider)    x.setAttribute("type", "range");  - min & max attributes + step attr (double check all attr and compatability)
+
+- img
+- textarea
+- header
+- label
+- select (for dropdown)
+
+- node server.js to run 2nd server - must do "server" to initially run html 
+
+*/
+
+
+
+
+
+
+
+
