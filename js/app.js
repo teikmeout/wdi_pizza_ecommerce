a40aa5d2ad3523cf111405413150bffc14051197
Dawa Sherpa <dasherpa7@gmail.com>
2016-09-28 13:52:11 -0400

    submit button takes u to checkout page

--- js/app.js
+++ js/app.js
@@ -1,35 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
   console.log('this is after the dom loaded');
-
   var $listForm = document.querySelector('.list-maker-form');
 
+
+  // var $serial = $('form').serialize();
+  // console.log($serial);
+
 });
+
 
 console.log('this is before the dom loaded');
 
 
-/* Elements to use
-Test elements in safari
-
-- form
-- button
-- input type=checkbox (for checkbox)
-- input type=range (for slider)    x.setAttribute("type", "range");  - min & max attributes + step attr (double check all attr and compatability)
-
-- img
-- textarea
-- header
-- label
-- select (for dropdown)
-
-- node server.js to run 2nd server - must do "server" to initially run html 
-
-*/
 
 
 
-
-
+// function httpGet(theUrl)
+// {
+//     var xmlHttp = new XMLHttpRequest();
+//     xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+//     xmlHttp.send( null );
+//     return xmlHttp.responseText;
+// }
 
 
 
