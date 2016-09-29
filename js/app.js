87e6eb5506d6f637c18da84f66338745c4cfeaa5
Dawa Sherpa <dasherpa7@gmail.com>
2016-09-29 19:28:18 -0400

    refactored final code

--- js/app.js
+++ js/app.js
@@ -1,70 +1,24 @@
+var $listForm = document.querySelector('.list-maker-form');
+
+// store image selection via get
+var image = window.location.search.split('=')[1].match(/[a-zA-Z]+/)[0];
+function storeImageInfo() {
+  return image;
+}
+
+function whatSize() {
+  if (storeImageInfo() === 'small') {
+    $('#size select [value=small]').attr('selected', 'selected');
+  } else if (storeImageInfo() === 'medium') {
+    $('#size select [value=medium]').attr('selected', 'selected');
+  } else {
+    $('#size select [value=large]').attr('selected', 'selected');
+  }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   console.log('this is after the dom loaded');
-  var $listForm = document.querySelector('.list-maker-form');
-
-  var $homepageSubmit = $('#homepageSubmit');
-
-  var name = window.location.search.split('=')[1];
-
-  function storeInfo() {
-    return name;
-  }
-
-  var first = large = window.location.search.split('=')[1].split('+')[0]
-  "large";
-  var second = large = window.location.search.split('=')[1].split('+')[1].split('&')[0];
-
-
-  var image = window.location.search.split('=')[1].match(/[a-zA-Z]+/)[0];
-
-  function storeImageInfo() {
-    return image;
-  }
-
-
-
-  $('#checkoutFirstName').attr('value', storeInfo());
-
-
-  function whatSize() {
-    if (storeImageInfo() === 'small') {
-      $('#size select [value=small]').attr('selected', 'selected');
-    } else if (storeImageInfo() === 'medium') {
-      $('#size select [value=medium]').attr('selected', 'selected');
-    } else {
-      $('#size select [value=large]').attr('selected', 'selected');
-    }
-  }
-
   whatSize();
-
-});
+})
 
 console.log('this is before the dom loaded');
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // var $serial = $('form').serialize();
-  // console.log($serial);
-
-
-// function httpGet(theUrl)
-// {
-//     var xmlHttp = new XMLHttpRequest();
-//     xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-//     xmlHttp.send( null );
-//     return xmlHttp.responseText;
-// }
-
-
-
